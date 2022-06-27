10331e70229856cd091f3af645f50d824837cc18
Sajib Mandal <107785806+sajib-mandal@users.noreply.github.com>
2022-06-27 12:02:28 +0530

    Create MergeSort.py

---
+++ Sorting/MergeSort.py
@@ -0,0 +1,32 @@
+def mergeSort(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        L = arr[:mid]
+        R = arr[mid:]
+        mergeSort(L)
+        mergeSort(R)
+
+        i = j = k = 0
+        while i < len(L) and j < len(R):
+            if L[i] < R[j]:
+                arr[k] = L[i]
+                i += 1
+            else:
+                arr[k] = R[j]
+                j += 1
+            k += 1
+
+        # Checking if element was left
+        while i < len(L):
+            arr[k] = L[i]
+            i += 1
+            k += 1
+        while j < len(R):
+            arr[k] = R[j]
+            j += 1
+            k += 1
+
+
+arr = [2, 4, 5, 7, 1, 2, 3, 6]
+mergeSort(arr)
+print(arr)
