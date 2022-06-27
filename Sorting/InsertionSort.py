afa7292a834936487cf108b6afebffbe98c5651a
Sajib Mandal <107785806+sajib-mandal@users.noreply.github.com>
2022-06-27 12:01:22 +0530

    Create InsertionSort.py

---
+++ Sorting/InsertionSort.py
@@ -0,0 +1,16 @@
+nes (12 sloc)  283 Bytes
+
+def insertion_sort(arr):
+    n = len(arr)
+    for j in range(1, n):
+        key = arr[j]
+
+        i = j - 1
+        while i >= 0 and arr[i] > key:
+            arr[i + 1] = arr[i]
+            i = i - 1
+        arr[i + 1] = key
+
+arr = [5, 2, 4, 6, 1, 3]
+insertion_sort(arr)
+print(arr)
