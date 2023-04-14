b0d5025214b9144021914d033a50c765720ab011
Sajib Mandal <107785806+sajib-mandal@users.noreply.github.com>
2023-04-14 22:00:42 +0530

    Update binary_search.py

--- Binary Search/binary_search.py
+++ Binary Search/binary_search.py
@@ -1,5 +1,4 @@
 # Recursive
-
 def binary_search(arr, target):
     start = 0
     end = len(arr) - 1
@@ -17,6 +16,36 @@ def binary_search(arr, target):
             return search(arr, target, mid + 1, end)
     return search(arr, target, start, end)
 
+
+
+print(binary_search([1, 2, 3, 4, 5], 1)) # 0
+print(binary_search([1, 2, 3, 4, 5], 2)) # 1
+print(binary_search([1, 2, 3, 4, 5], 3)) # 2
+print(binary_search([1, 2, 3, 4, 5], 4)) # 3
+print(binary_search([1, 2, 3, 4, 5], 5)) # 4
+print(binary_search([1, 2, 3, 4, 5], 6)) # -1
+
+
+
+
+
+
+
+# Iterative
+def binary_search(arr, target):
+    start = 0
+    end = len(arr) - 1
+    
+    while start <= end:
+        mid = (start + end) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            start = mid + 1
+        else:
+            end = mid - 1
+
+    return -1
 
 
 print(binary_search([1, 2, 3, 4, 5], 1)) # 0
